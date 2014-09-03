4ac562a68a3d18b7b42facb9600ddff9c6c58034
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-09-04 08:52:17 +0900

    fix indent

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ var colors = util.inspect.colors;
 var colo = {};
 
 var deco = function(_styles) {
-  var decorator = function() {
-    return decorate.apply(decorator, arguments);
+  var decorator = function(str) {
+    return decorate.apply(decorator, [str]);
   };
   decorator._styles = _styles;
   decorator.__proto__ = Object.defineProperties(function(){}, styles);
   return decorator;
-}
+};
 
-var decorate = function() {
-  var result = arguments[0];
+var decorate = function(str) {
+  var result = str;
   this._styles.forEach(function(style) {
     var color = colors[style];
     if (color) {
@@ -21,17 +21,17 @@ var decorate = function() {
     }
   });
   return result;
-}
+};
 
 var styles = (function () {
   var result = {};
   Object.keys(colors).forEach(function (color) {
-  result[color] = {
-    get: function () {
-      if (!this._styles) this._styles = [];
-      return deco(this._styles.concat(color));
-    }
-  };
+    result[color] = {
+      get: function () {
+        if (!this._styles) this._styles = [];
+        return deco(this._styles.concat(color));
+      }
+    };
   });
   return result;
 })();
